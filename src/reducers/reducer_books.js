2ee8049a3f7b4fafd576bd8c52fb2ed55558b5fd
Brandon <railsdev14@gmail.com>
2017-04-11 13:58:52 -0400

    First commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+	return [
+		{ title: 'book1', pages: 250},
+		{ title: 'book2', pages: 101},
+		{ title: 'book3', pages: 394},
+		{ title: 'book4', pages: 127},
+		{ title: 'book5', pages: 298}
+	]
+}
